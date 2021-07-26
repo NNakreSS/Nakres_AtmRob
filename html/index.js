cf3acc50d9717b9e7bc7f8662266e6918d997c64
NNakreSS <87872407+NNakreSS@users.noreply.github.com>
2021-07-26 18:16:19 +0300

    Update index.js

--- html/index.js
+++ html/index.js
@@ -21,35 +21,35 @@ $(function () {
     })
     document.onkeyup = function (data) {
         if (data.which == 27) {
-            $.post('http://nakres_atmrob/exit', JSON.stringify({}));
+            $.post('http://Nakres_AtmRob/exit', JSON.stringify({}));
             return
         }
     };
     $("#close").click(function () {
-        $.post('http://nakres_atmrob/exit', JSON.stringify({}));
+        $.post('http://Nakres_AtmRob/exit', JSON.stringify({}));
         return
     })
     $("#submit").click(function () {
         let inputValue = $("#input").val()
         if (inputValue.length >= 100) {
-            $.post("http://nakres_atmrob/error", JSON.stringify({
+            $.post("http://Nakres_AtmRob/error", JSON.stringify({
                 error: "Input was greater than 100"
             }))
             return
         } else if (!inputValue) {
-            $.post("http://nakres_atmrob/error", JSON.stringify({
+            $.post("http://Nakres_AtmRob/error", JSON.stringify({
                 error: "There was no value in the input field"
             }))
             return
         }
         if (inputValue == "SUT-HOLEZ"){
-        $.post('http://nakres_atmrob/code', JSON.stringify({
+        $.post('http://Nakres_AtmRob/code', JSON.stringify({
             password: inputValue,
         }));
         return;
         } 
             else {
-            $.post("http://nakres_atmrob/error", JSON.stringify({
+            $.post("http://Nakres_AtmRob/error", JSON.stringify({
                     error: "Incorrect password"
                 }))
                 return
